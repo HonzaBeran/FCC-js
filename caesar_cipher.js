75399277474624e5e1b6d7fca51ec2447b4a596d
Honza Beran <beranjan97@gmail.com>
2022-06-05 18:06:54 +0200

    Added function checkUpper. (checkUpper(str) -> arr) Function check if char is upper and in the position of char in string push boolean into array.

--- caesar_cipher.js
+++ caesar_cipher.js
@@ -1,29 +1,53 @@
 function rot13(str) {
-    // 
-  }
+    const isUpper = checkUpper(str);
+    let strLower = str.toLowerCase();
+    console.log(lowerShift(strLower, 13));
+    /*
+    if (strArrLower.length !== strLower.length) {
+        console.log("Break!")
+        return undefined;
+    }
+    for (let i = 0; i < strArrLower.length; i++) {
+        //
+    }
+    */
+    console.log("OK");
+    // return strArrLower;
+}
 
 const lowerShift = (str, numShift) => {
     const maxLower = 122;
     const minLower = 97;
     const strArr = [];
-    console.log(strArr);
     let charCode;
     for (let i = 0; i < str.length; i++) {
         charCode = str.charCodeAt(i);
         // console.log(charCode);
         if (charCode <= maxLower && charCode >= minLower) {
-            if (charCode + 13 <= maxLower) {
-                strArr.push(String.fromCharCode(charCode + 13));
+            if (charCode + numShift <= maxLower) {
+                strArr.push(String.fromCharCode(charCode + numShift));
             } else {
-                strArr.push(String.fromCharCode(minLower + (charCode + 13 - maxLower) - 1));
+                strArr.push(String.fromCharCode(minLower + (charCode + numShift - maxLower) - 1));
             }
         } else {
             strArr.push(String.fromCharCode(charCode));
         }
     }
-    console.log(strArr.join(''));
+    return strArr.join('');
 }
 
-lowerShift("SERR PBQR PNZC".toLowerCase());
+const checkUpper = (str) => {
+    let lowerArr = [];
+    for (let i = 0; i < str.length; i++) {
+        if (/[A-Z]/.test(str[i])) {
+            lowerArr.push(true);
+        } else {
+            lowerArr.push(false);
+        }
+    }
+    return lowerArr;
+}
+
+// lowerShift("SERR PBQR PNZC");
   
-// rot13("SERR PBQR PNZC");
+rot13("SERR PBQR PNZC");
