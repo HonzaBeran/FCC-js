6cecdf3ed06c8cc34e61dd1a51728bad8c8d3e84
Honza Beran <beranjan97@gmail.com>
2022-06-05 15:03:25 +0200

    Decompozition problem to fce(rot13) and fce(lowerWhift). lowerShift makes shift for lowerCase chars.

--- caesar_cipher.js
+++ caesar_cipher.js
@@ -1,12 +1,29 @@
 function rot13(str) {
-    const maxUpper = 90;
-    const minUpper = 65;
+    // 
+  }
+
+const lowerShift = (str, numShift) => {
     const maxLower = 122;
     const minLower = 97;
+    const strArr = [];
+    console.log(strArr);
+    let charCode;
     for (let i = 0; i < str.length; i++) {
-        console.log(str[i]);
+        charCode = str.charCodeAt(i);
+        // console.log(charCode);
+        if (charCode <= maxLower && charCode >= minLower) {
+            if (charCode + 13 <= maxLower) {
+                strArr.push(String.fromCharCode(charCode + 13));
+            } else {
+                strArr.push(String.fromCharCode(minLower + (charCode + 13 - maxLower) - 1));
+            }
+        } else {
+            strArr.push(String.fromCharCode(charCode));
+        }
     }
-    return str;
-  }
+    console.log(strArr.join(''));
+}
+
+lowerShift("SERR PBQR PNZC".toLowerCase());
   
-  rot13("SERR PBQR PNZC");
+// rot13("SERR PBQR PNZC");
