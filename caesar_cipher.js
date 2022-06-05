a81191ede53faac1888fb04359bb6440961ac4a7
Honza Beran <beranjan97@gmail.com>
2022-06-05 13:22:11 +0200

    Caesar cipher <- added.

---
+++ caesar_cipher.js
@@ -0,0 +1,12 @@
+function rot13(str) {
+    const maxUpper = 90;
+    const minUpper = 65;
+    const maxLower = 122;
+    const minLower = 97;
+    for (let i = 0; i < str.length; i++) {
+        console.log(str[i]);
+    }
+    return str;
+  }
+  
+  rot13("SERR PBQR PNZC");
